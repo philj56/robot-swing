a40e8acef0426b33f3b09a526494777adc85a0c8
harry <harryjacobs9@gmail.com>
2016-01-26 14:21:44 +0000

    Example movemet program

---
+++ sdk/movehead/build-harry/CMakeFiles/movehead.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/movehead.dir/movehead.cpp.o"
+  "sdk/bin/movehead.pdb"
+  "sdk/bin/movehead"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/movehead.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
