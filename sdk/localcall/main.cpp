e18f63eebd9e1b92dc4bd1ea6e0d85cc909f538b
Phil Jones <pjj313@student.bham.ac.uk>
2016-02-02 13:51:16 +0000

    Added extra localcall options

--- sdk/localcall/main.cpp
+++ sdk/localcall/main.cpp
@@ -34,14 +34,15 @@ void argErr(void)
 {
 	// Standard usage message
 	std::string usage = "Usage: localcall [--pip robot_ip] [--pport port] [--lib library] [--mod module] [--fun function]";
-	
-	std::cerr << "Wrong number of arguments!" << std::endl;
 	std::cerr << usage << std::endl;
 	exit(2);
 }
 
 int main(int argc, char* argv[])
 {
+	// Whether to print verbose output - default off
+	bool verb = false;
+
 	// Default name of desired library
 	std::string libName = "libmoduletest.so";
 	
@@ -61,17 +62,19 @@ int main(int argc, char* argv[])
 	std::string pip = "127.0.0.1";
 	
 	// Check for odd number of command line arguments (in this case)
-	if (argc % 2 != 1)
+/*	if (argc % 2 != 1)
 	{
 		argErr();
 	}
-	
+*/	
 	// Get any arguments
 	while (true)
 	{
 		static int index = 0;
 
 		// Struct of options
+		// Columns are:
+		// 	{Option name,	Option required?,	flag(not needed here),	return value/character}
 		static const struct option longopts[] =
 		{
 			{"pip", 	1, 	0, 	'p'},
@@ -79,41 +82,56 @@ int main(int argc, char* argv[])
 			{"lib",		1,	0,	'l'},
 			{"mod",		1,	0,	'm'},
 			{"fun",		1,	0,	'f'},
+			{"verb",	0,	0,	'v'},
+			{"help",	0,	0,	'h'},
 			{0,		0,	0,	 0 }
 		};
 
-		switch(index = getopt_long(argc, argv, "", longopts, &index))
+		// Get next option, and check return value
+		// p:o:l:m:f:vh allow short option specification
+		switch(index = getopt_long(argc, argv, "p:o:l:m:f:vh", longopts, &index))
 		{
+			// Print usage and quit
+			case 'h':
+				argErr();
+				break;
+			// Set parent IP
 			case 'p':
 				if (optarg)
 					pip = std::string(optarg);
 				else
 					argErr();
 				break;
+			// Set parent port
 			case 'o':
 				if (optarg)
 					pport = atoi(optarg);
 				else
 					argErr();
 				break;
+			// Set library name
 			case 'l':
 				if (optarg)
 					libName = std::string(optarg);
 				else
 					argErr();
 				break;
+			// Set module name
 			case 'm':
 				if (optarg)
 					moduleName = std::string(optarg);
 				else
 					argErr();
 				break;
+			// Set function name
 			case 'f':
 				if (optarg)
 					funcName = std::string(optarg);
 				else
 					argErr();
 				break;
+			case 'v':
+				verb = true;
 		}
 		if (index == -1)
 			break;
@@ -123,6 +141,8 @@ int main(int argc, char* argv[])
 	setlocale(LC_NUMERIC, "C");
 
 	// Create a broker
+	if(verb)
+		std::cout << bold_on << "Creating broker..." << bold_off << std::endl;
 	boost::shared_ptr<AL::ALBroker> broker;
 	try
 	{
@@ -153,11 +173,13 @@ int main(int argc, char* argv[])
 	AL::ALBrokerManager::getInstance()->addBroker(broker);
 
 	// Find the desired library
-	std::cout << bold_on << "Finding " << libName << "..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Finding " << libName << "..." << bold_off << std::endl;
 	std::string library = qi::path::findLib(libName.c_str());
 
 	// Open the library
-	std::cout << bold_on << "Loading " << library << "..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Loading " << library << "..." << bold_off << std::endl;
 	void* handle = qi::os::dlopen(library.c_str());
 	if (!handle)
 	{
@@ -168,7 +190,8 @@ int main(int argc, char* argv[])
 	}
 
 	// Load the create symbol
-	std::cout << bold_on << "Loading _createModule symbol..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Loading _createModule symbol..." << bold_off << std::endl;
 	int(*createModule)(boost::shared_ptr<AL::ALBroker>) = (int(*)(boost::shared_ptr<AL::ALBroker>))(qi::os::dlsym(handle, "_createModule"));
 	if (!createModule)
 	{
@@ -179,31 +202,40 @@ int main(int argc, char* argv[])
 	}
 
 	// Check if module is already present
-	std::cout << bold_on << "Module " << moduleName << " is ";
-	if (!(broker->isModulePresent(moduleName)))
+	if(verb)
 	{
-		std::cout << "not ";
+		std::cout << bold_on << "Module " << moduleName << " is ";
+		if (!(broker->isModulePresent(moduleName)))
+		{
+			std::cout << "not ";
+		}
+		std::cout << "present" << bold_off << std::endl;
 	}
-	std::cout << "present" << bold_off << std::endl;
 
 	// Create an instance of the desired module
-	std::cout << bold_on << "Creating " << moduleName << " instance..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Creating " << moduleName << " instance..." << bold_off << std::endl;
 	createModule(broker);
 
 	// Check for module creation
-	std::cout << bold_on << "Module " << moduleName.c_str() << " is ";
-	if (!(broker->isModulePresent(moduleName)))
+	if(verb)
 	{
-		std::cout << "not ";
+		std::cout << bold_on << "Module " << moduleName.c_str() << " is ";
+		if (!(broker->isModulePresent(moduleName)))
+		{
+			std::cout << "not ";
+		}
+		std::cout << "present" << bold_off << std::endl;
 	}
-	std::cout << "present" << bold_off << std::endl;
-	
+
 	// Create a proxy to the module	
-	std::cout << bold_on << "Creating proxy to module..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Creating proxy to module..." << bold_off << std::endl;
 	AL::ALProxy testProxy(moduleName, pip, pport);
 	
 	// Call a generic function from the module with no arguments
-	std::cout << bold_on << "Calling function " << funcName << "..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Calling function " << funcName << "..." << bold_off << std::endl;
 	testProxy.callVoid(funcName);
 
 	// Get a handle to the module and close it
@@ -214,19 +246,24 @@ int main(int argc, char* argv[])
 	}
 
 	// Check module has closed
-	std::cout << bold_on << "Module " << moduleName << " is ";
-	if (!(broker->isModulePresent(moduleName)))
+	if(verb)
 	{
-		std::cout << "not ";
+		std::cout << bold_on << "Module " << moduleName << " is ";
+		if (!(broker->isModulePresent(moduleName)))
+		{
+			std::cout << "not ";
+		}
+		std::cout << "present" << bold_off << std::endl;
 	}
-	std::cout << "present" << bold_off << std::endl;
-
+	
 	// Close the broker
-	std::cout << bold_on << "Closing broker..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Closing broker..." << bold_off << std::endl;
 	broker->shutdown();
 
 	// Exit program
-	std::cout << bold_on << "Exiting..." << bold_off << std::endl;
+	if(verb)
+		std::cout << bold_on << "Exiting..." << bold_off << std::endl;
 
 	return 0;
 }
