99522843e303a9b8a17f70653bcd7bd1d3d747e7
Ben <benjamin.stks@gmail.com>
2016-02-23 16:41:21 +0000

    Update Experience.cpp

--- machine-learning/Experience.cpp
+++ machine-learning/Experience.cpp
@@ -1,5 +1,8 @@
 #include "Experience.h"
 
-Experience::Experience(const Action* const _action):
+Experience::Experience(const Action* const _action, const double x, const double y):
 action(_action)
-{}
+{
+  state[0]=x;
+  state[1]=y;
+}
