8308db2511a98e05515d89ebf3f1f9da0f9edb28
Ben <benjamin.stks@gmail.com>
2016-02-23 16:38:59 +0000

    attempt to improve main loop arrangement

--- machine-learning/Main.cpp
+++ machine-learning/Main.cpp
@@ -17,13 +17,13 @@ int main()
 	//array to hold the current state of the robot
 	double state[2]={0,0};
 	
-	//a pointer to the last experience
-	Experience* last_experience;
-	
-	//create pointers to the possible actions as well as a pointer to hold the chosen action
-	Action* actions[2],chosen_action;
+	//create pointers to the possible actions as well as pointers to hold the chosen action and the previous action
+	Action* actions[2],chosen_action,last_action;
 	actions[0]=new action(FORWARDS);
 	actions[1]=new action(BACKWARDS);
+	
+	//pointer to the previous queue
+	PriorityQueue<Experience*,double>* last_queue;
 	
 	while(true)
 	{
@@ -31,9 +31,6 @@ int main()
 		state[0]=getPosition();//!
 		state[1]=getVelocity();//!
 		
-		//add to last experience
-		last_experience.state[0]=state[0];
-		last_experience.state[1]=state[1];
 		
 		//get experiences at current point
 		PriorityQueue<Experience*,double>& experiences=space[state[0]][state[1]];
@@ -41,16 +38,16 @@ int main()
 		//find rewards//!
 		//choose acion//!
 		
-		//generate new experience
-		Experience* current_experience=new Experience(chosen_action);
-		experiences.push_back(current_experience);
-		//delete lowest value experience//!
+		//generate new experience based off last action
+		Experience* last_experience=new Experience(last_action,state[0],state[1]);
+		last_queue->enqueueWithPriority(last_experience,last_q);
+		last_queue=&experiences;
 		
 		//take action
 		chosen_action->execuate();
 		
 		//save last experience
-		last_experience=current_experience
+		last_action=current_action
 	}
 	
 	delete actions[0];
