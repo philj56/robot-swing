0386f49ccf7f8fbdfb0bdb03ebd1b998d898e632
Ben <benjamin.stks@gmail.com>
2016-02-23 15:25:50 +0000

    destructor written
    all experiences are now deleted along with the StateSpace object

--- machine-learning/StateSpace.cpp
+++ machine-learning/StateSpace.cpp
@@ -6,6 +6,23 @@ StateSpace::StateSpace(const int _x_size, const int _y_size):
 	space(x_size, std::vector< std::list<Experience*> > (y_size))
 {}
 
+StateSpace::~StateSpace()
+{
+	//ridiculously extensive clean-up
+	for(unsigned int i=0 ; i<x_size ; ++i)
+	{
+		for(unsigned int j=0 ; j<x_size ; ++j)
+		{
+			std::vector<std::list<Experience*>>& list=space[i][j];
+			
+			for(std::list<Experience*>::iterator iter=list.begin(),end=list.end() ; iter!=end ; ++iter)
+			{
+				delete *iter;
+			}
+		}
+	}
+}
+
 SateSpace::SubscriptProxy SateSpace::operator[](const double index)
 {
 	//discretise index
