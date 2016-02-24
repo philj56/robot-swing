07189739cf74fba0153c2652bdd50ef57fff10de
owendiba <ohd303@student.bham.ac.uk>
2016-02-24 23:44:02 +0000

    Added files via upload
    function for selecting actions based on Q values and Boltzmann probability distribution

---
+++ machine-learning/selectAction.h
@@ -0,0 +1,46 @@
+//function is fed with a priority queue of action-values 
+//generates Boltzmann distribution of these action-values
+//and selects an action based on probabilities 
+Experience * selectAction(PriorityQueue<Experience *,double> a_queue, double temp)
+{	
+    typedef PriorityQueue<Experience *,double> PQ;
+    typedef std::vector< std::pair<Experience *, double> > Vec_Pair;
+    typedef std::pair<Experience *, double> Pair;
+    
+    double sum= 0;
+    int i = 0;
+    
+    int size = a_queue.getSize();
+    Vec_Pair action_vec(size);
+    Pair pear;
+    
+    //Calculate partition function by iterating over action-values
+	for(PQ::constIter iter = a_queue.begin(); iter < a_queue.end(); iter++)
+	{
+		sum += std::exp((iter->second)/temp);
+	}
+	//Calculate boltzmann factors for action-values
+	for(Vec_Pair::iterator it = action_vec.begin(); it < action_vec.end(); it++)
+	{
+	    it->first = a_queue[i].first;
+	    it->second = std::exp(a_queue[i].second / temp) / sum;
+	    i++;
+	}
+	//calculate cumulative probability distribution    
+    for(std::vector< std::pair<int, double> >::iterator it = action_vec.begin()+1; it < action_vec.end(); it++)
+	{
+	    it->second += (it-1)->second;
+	}
+    //seed random number generator
+	srand(time(NULL));
+	//generate RN between 0 and 1 
+	double rand_num = (double)rand()/ (RAND_MAX);
+	
+	//select action based on probability 
+    for(Vec_Pair::iterator it = action_vec.begin(); it < action_vec.end(); it++)
+    {
+        //if RN falls within cumulative probability bin return the corresponding action
+        if(rand_num < it->second){return it->first};
+    }
+    return -1;
+}
