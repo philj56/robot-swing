3eb0f37a0da8a5834c7b12b4aa27b59a5aff281c
Ben <benjamin.stks@gmail.com>
2016-02-25 15:22:05 +0000

    updated to use action objects
    rip experience object

--- machine-learning/StateSpace.cpp
+++ machine-learning/StateSpace.cpp
@@ -3,7 +3,7 @@
 StateSpace::StateSpace(const int _x_size, const int _y_size):
 	x_size(_x_size),
 	y_size(_y_size),
-	space(x_size, std::vector< PriorityQueue<Experience*,double> > (y_size, PriorityQueue<Experience*,double> (HeapType::MAX)))
+	space(x_size, std::vector< PriorityQueue<Action*,double> > (y_size, PriorityQueue<Action*,double> (HeapType::MAX)))
 {}
 
 StateSpace::~StateSpace()
@@ -13,7 +13,7 @@ StateSpace::~StateSpace()
 	{
 		for(unsigned int j=0 ; j<x_size ; ++j)
 		{
-			PriorityQueue<Experience*,double>& queue=space[i][j];
+			PriorityQueue<Action*,double>& queue=space[i][j];
 			
 			for(auto iter=queue.begin(),queue=list.end() ; iter!=end ; ++iter)
 			{
