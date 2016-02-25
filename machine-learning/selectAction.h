bf0deb2dffa9343af001341965d844175ecbac61
Ben <benjamin.stks@gmail.com>
2016-02-25 14:15:31 +0000

    Update selectAction.h

--- machine-learning/selectAction.h
+++ machine-learning/selectAction.h
@@ -1,7 +1,7 @@
 //function is fed with a priority queue of action-values 
 //generates Boltzmann distribution of these action-values
 //and selects an action based on probabilities 
-Experience * selectAction(PriorityQueue<Experience *,double> a_queue, double temp)
+Experience * selectAction(PriorityQueue<Experience *,double>& a_queue, double temp)
 {	
 	typedef PriorityQueue<Experience *,double> PQ;
 	typedef std::vector< std::pair<Experience *, double> > Vec_Pair;
@@ -15,34 +15,30 @@ Experience * selectAction(PriorityQueue<Experience *,double> a_queue, double tem
 	Pair pear;
 	
 	//Calculate partition function by iterating over action-values
-	for(PQ::constIter iter = a_queue.begin(); iter < a_queue.end(); iter++)
+	for(PQ::constIter iter = a_queue.begin(),end=a_queue.end(); iter < end; ++iter)
 	{
-		sum += std::exp(((*iter)->second)/temp);
+		sum += std::exp((iter->second)/temp);
 	}
 	//Calculate boltzmann factors for action-values
-	for(Vec_Pair::iterator it = action_vec.begin(); it < action_vec.end(); it++)
+	for(Vec_Pair::iterator it = action_vec.begin(),end=action_vec.end(); it < end; ++it)
 	{
-	    (*it)->first = a_queue[i].first;
-	    (*it)->second = std::exp(a_queue[i].second / temp) / sum;
-	    i++;
+	    it->first = a_queue[i].first;
+	    it->second = std::exp(a_queue[i].second / temp) / sum;
+	    ++i;
 	}
 	//calculate cumulative probability distribution    
-	for(std::vector< std::pair<int, double> >::iterator it = action_vec.begin()+1; it < action_vec.end(); it++)
+	for(std::vector< std::pair<int, double> >::iterator it = action_vec.begin()+1,end=action_vec.end(); it < end; ++it)
 	{
-	    (*it)->second += (it-1)->second;
+	    it->second += (it-1)->second;
 	}
 	
-	//seed random number generator		!-----may be done in main-----!
-	srand(time(NULL));
 	//generate RN between 0 and 1
 	double rand_num = (double)rand()/ (RAND_MAX);
 	
 	//select action based on probability 
-	for(Vec_Pair::iterator it = action_vec.begin(); it < action_vec.end(); it++)
+	for(Vec_Pair::iterator it = action_vec.begin(),end=action_vec.end(); it < end; ++it)
 	{
 		//if RN falls within cumulative probability bin return the corresponding action
-		if(rand_num < (*it)->second)return (*it)->first;
+		if(rand_num < it->second)return it->first;
 	}
-	
-	return -1;
 }
