c67893a1646a52194e451895d9110612d545bd93
Ben <benjamin.stks@gmail.com>
2016-02-26 16:42:19 +0000

    made into a struct

--- machine-learning/state.h
+++ machine-learning/state.h
@@ -5,27 +5,22 @@
 
 //class defines state object that holds angular position and velocity, and 
 //robot state. Has functions to calculate reward for a state
-class State
+struct State
 {
-    private: 
-        double theta;
-        double theta_dot;
-        enum ROBOT_STATE {FORWARD, BACKWARD};
-        const double g = 9.81;
-    public:
-        State(double _theta, double _theta_dot, enum _RobotState):
-            theta(_theta),
-            theta_dot(_theta_dot),
-            RobotState(_RobotState)
-        {}
+    double theta;
+    double theta_dot;
+    enum ROBOT_STATE {FORWARD, BACKWARD};
+    static const double g = 9.81;
+    
+    State(double _theta, double _theta_dot, enum _RobotState):
+        theta(_theta),
+        theta_dot(_theta_dot),
+        RobotState(_RobotState)
+    {}
         
-        double getReward(){
-            g * std::cos(theta) + 0.5 * theta_dot * theta_dot;
-        }
-        
-        double getTheta(){return theta;}
-        double getThetaDot(){return theta_dot;}
-        enum getRobotState(){return RobotState;}
+    double getReward(){
+        g * std::cos(theta) + 0.5 * theta_dot * theta_dot;
+    }
 };
 
 #endif
