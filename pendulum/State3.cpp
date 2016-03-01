9c78eef6570d5fbb9e8efa85f4220106c778f83f
zlhodgins <zlh312@student.bham.ac.uk>
2016-03-01 15:49:37 +0000

    Create State3.cpp

---
+++ pendulum/State3.cpp
@@ -0,0 +1,14 @@
+//for pendulum
+
+#include "State3.h"
+
+State::State(double _theta, double _theta_dot, double _torque):
+  theta(_theta),
+  theta_dot(_theta_dot),
+  torque(_robot_state)
+{}
+
+double State::getReward(){
+  //reward proportional to theta
+  return theta;
+}
