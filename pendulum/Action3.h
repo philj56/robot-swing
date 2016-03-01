1590229bbc181ecf4c2929744334051a4e333d42
zlhodgins <zlh312@student.bham.ac.uk>
2016-03-01 16:01:55 +0000

    Create Action3.h

---
+++ pendulum/Action3.h
@@ -0,0 +1,23 @@
+#ifndef ACTION_H
+#define ACTION_H
+
+//struct to store a basic action
+//currently just stores an integer identifier - more will be added later
+
+struct Action
+{
+	explicit Action(const int _action);
+	
+	//numerical identifier for the action
+	const int action;
+	
+	//function to call execute the associated action
+	void exectute();
+	
+	//needs function pointer to actual action
+	
+	//comparison operator for use by priority queue
+	bool operator==(const Action& target) const;
+};
+
+#endif
