c056e518ca11581399aac3afeccdf68728aa5856
Ben <benjamin.stks@gmail.com>
2016-03-01 12:27:54 +0000

    more comments

--- machine-learning/State.cpp
+++ machine-learning/State.cpp
@@ -7,5 +7,6 @@ State::State(double _theta, double _theta_dot, ROBOT_STATE _robot_state):
 {}
 
 double State::getReward(){
+  //return kinetic + potential
   return g * std::cos(theta) + 0.5 * theta_dot * theta_dot;
 }
