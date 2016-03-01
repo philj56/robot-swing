b3c22fed0a26f88ecddc1907935297a3487a56df
Ben <benjamin.stks@gmail.com>
2016-03-01 16:53:01 +0000

    update from ML folder

--- pendulum/Action3.cpp
+++ pendulum/Action3.cpp
@@ -1,16 +1,22 @@
+
 #include "Action.h"
 
-Action::Action(const int _action):
-action(_action)
+Action::Action(const int _action, void (*)() _execute):
+action(_action), execute(_execute)
 {}
 
-Action::Execute()
-{
-  //call stored function
-}
-
-bool operator==(const Action& a1) const
+bool operator==(const Action& target) const
 {
   //two actions must be equal if their identifiers are equal
   return action==target.action;
+}
+
+bool operator!=(const Action& target) const 
+{
+  return !(*this == target);
+}
+
+const Action& operator=(const Action& target)
+{
+  action = target.action;
 }
