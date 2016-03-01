aafa100e62e0a01b990131025bb1dde62f608e3c
SJR276 <sjrowlinson@outlook.com>
2016-03-01 14:59:15 +0000

    Implemented operator!= definition

--- machine-learning/Action.cpp
+++ machine-learning/Action.cpp
@@ -4,13 +4,18 @@ Action::Action(const int _action):
 action(_action)
 {}
 
-Action::Execute()
+void Action::execute()
 {
   //call stored function
 }
 
-bool operator==(const Action& a1) const
+bool operator==(const Action& target) const
 {
   //two actions must be equal if their identifiers are equal
   return action==target.action;
+}
+
+bool operator!=(const Action& target) const 
+{
+  return !(*this == target);
 }
