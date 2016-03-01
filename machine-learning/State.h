e276cc1a626ff4f3d198528abd00b956c97df957
SJR276 <sjrowlinson@outlook.com>
2016-03-01 15:41:18 +0000

    made horrendous global variable - can be changed if necessary

--- machine-learning/State.h
+++ machine-learning/State.h
@@ -4,6 +4,7 @@
 #include <cmath>
 
  enum ROBOT_STATE {FORWARD, BACKWARD};
+ const double g = 9.81;
 
 //class defines state object that holds angular position and velocity, and 
 //robot state. Has functions to calculate reward for a state
@@ -14,7 +15,7 @@ struct State
     double theta_dot;
     ROBOT_STATE robot_state;
     
-    static const double g = 9.81;
+    //static const double g = 9.81;
     
     State(double _theta, double _theta_dot, ROBOT_STATE _robot_state);
     
