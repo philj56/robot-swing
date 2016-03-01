141cb03f48a6268c45fb8ab69c35cf700d11bce8
SJR276 <sjrowlinson@outlook.com>
2016-03-01 15:03:06 +0000

    Added assignment operator declaration

--- machine-learning/Action.h
+++ machine-learning/Action.h
@@ -19,7 +19,19 @@ struct Action
 	//comparison operator for use by priority queue
 	bool operator==(const Action& target) const;
 	
+	/**
+	 * @brief Overloaded inequivalent operator
+	 * 
+	 * @param target action object to compare
+	 */
 	bool operator!=(const Action& target) const;
+	
+	/**
+	 * @brief Overloaded assignment operator
+	 * 
+	 * @param target action object to assign to this
+	 */
+	const Action& operator=(const Action& target);
 };
 
 #endif
