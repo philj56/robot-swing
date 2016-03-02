635190cdf836014a9155ee8c9684898e55e67413
root <root@gs.(none)>
2016-03-02 06:56:40 -0500

    broke something

---
+++ sdk/robotencoder/build-tc/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /gs/naoqi/doc/examples/new/robot-swing/sdk/robotencoder
+# Build directory: /gs/naoqi/doc/examples/new/robot-swing/sdk/robotencoder/build-tc
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(test_robotencoder "/gs/naoqi/doc/examples/new/robot-swing/sdk/robotencoder/build-tc/sdk/bin/test_robotencoder")
+SET_TESTS_PROPERTIES(test_robotencoder PROPERTIES  TIMEOUT "20")
