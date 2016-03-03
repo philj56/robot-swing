ded30ad20fb031a788794dc8ab2dd32f6e4542d0
ml <ml@gs.(none)>
2016-03-03 12:19:12 -0500

    added machine learning program

---
+++ sdk/machinelearning/build-ml/CMakeFiles/machinelearning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/machinelearning.dir/main.cpp.o"
+  "sdk/bin/machinelearning.pdb"
+  "sdk/bin/machinelearning"
+  "CMakeFiles/CMakeRelink.dir/machinelearning.pdb"
+  "CMakeFiles/CMakeRelink.dir/machinelearning"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/machinelearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ sdk/machinelearning/build-ml/CMakeFiles/test_machinelearning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_machinelearning.dir/test.cpp.o"
+  "sdk/bin/test_machinelearning.pdb"
+  "sdk/bin/test_machinelearning"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_machinelearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
