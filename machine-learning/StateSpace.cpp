4468a8324bf265d60fcd419e400072a118526028
owendiba <ohd303@student.bham.ac.uk>
2016-03-04 11:37:08 +0000

    changed action * to int*

--- machine-learning/StateSpace.cpp
+++ machine-learning/StateSpace.cpp
@@ -2,12 +2,12 @@
 
 // NOTE: cannot initialise angle_bins or velocity_bins here as they are static. Also space is not a field of StateSpace and
 // _angle_max and _velocity_max are not declared.
-StateSpace::StateSpace(PriorityQueue<Action*,double> queue ):
-	space1(_angle_max, std::vector< PriorityQueue<Action*,double> > (_velocity_max, PriorityQueue<Action*,double> (queue)))
-	space2(_angle_max, std::vector< PriorityQueue<Action*,double> > (_velocity_max, PriorityQueue<Action*,double> (queue)))
+StateSpace::StateSpace(PriorityQueue<int *,double> queue ):
+	space1(_angle_max, std::vector< PriorityQueue<int *,double> > (_velocity_max, PriorityQueue<int *,double> (queue)))
+	space2(_angle_max, std::vector< PriorityQueue<int *,double> > (_velocity_max, PriorityQueue<int *,double> (queue)))
 {}
 
-StateSpace::SubscriptProxy1 StateSpace::operator[](const unsigned int robot_state)
+StateSpace::SubscriptProxy1 StateSpace::operator[](const unsigned int * robot_state)
 {
 	//throw if the the index is out of bounds
 	if(robot_state>1)throw std::domain_error("action index exceeded");
@@ -16,7 +16,7 @@ StateSpace::SubscriptProxy1 StateSpace::operator[](const unsigned int robot_stat
 }
 
 //searches state space by state object
-PriorityQueue<Action *, double>& StateSpace::operator[](const State & state)
+PriorityQueue<int *, double>& StateSpace::operator[](const State & state)
 {
 	//call the subscript operators with the members of the state object
 	return (*this)[state.robot_state][state.theta][state.theta_dot];
