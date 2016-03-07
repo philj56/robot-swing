fa42d1d22c39e27662ea42d07e49ba30f35dd15f
Ben <benjamin.stks@gmail.com>
2016-03-07 14:27:08 +0000

    Update encoder.h

--- sdk/machinelearning/encoder.h
+++ sdk/machinelearning/encoder.h
@@ -24,8 +24,6 @@ class Encoder
         float old_angle;
         float velocity;
         
-        bool end_thread;
-        
         libusb_device_handle * handle;
         
         pthread_t thread;
