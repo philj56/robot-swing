a22e2b0177a89344d77169e7c52265ada52b6462
Ben <benjamin.stks@gmail.com>
2016-03-12 15:05:30 +0000

    Update Main.cpp

--- machine-learning/Main.cpp
+++ machine-learning/Main.cpp
@@ -1,213 +1,176 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include <string>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <sstream>
 #include "StateSpace.h"
 #include "PriorityQueue.h"
 #include "State.h"
-#include "encoder.h"
-#include "CreateModule.h"
-
-/**
- * @brief Gives a std::string representation of a primitive type
- * 
- * @param x Primitive type such as int, double, long
- * @return std::string conversion of param x
- */
-template<typename T> std::string to_string(T x) {
-	return static_cast<std::ostringstream&>((std::ostringstream() << std::dec << x)).str();
-}
+#include "Environment.h"
 
 //function to calculate a temperature for the select action function as a function of time
-double temperature();
+double temperature(unsigned long t);
 
 //function to select next action
-int selectAction(PriorityQueue<int,double>& a_queue);
+float selectAction(PriorityQueue<float, double>& a_queue, unsigned long iterations);
 
 //function to update a q value
-void updateQ(StateSpace & space, int action, State & new_state, State & old_state, double alpha, double gamma);
+void updateQ(StateSpace & space, float  action, State & new_state, State & old_state, double alpha, double gamma);
 
 int main()
-{	
-	// STUFF WE DONT UNDERSTAND, AND DONT NEED TO
-	//__________________________________________________________________________________________
-	//__________________________________________________________________________________________
-	// Libraries to load
-	std::string bodyLibName = "bodyinfo";
-	std::string movementLibName = "movementtools";
-	
-	// Name of camera module in library
-	std::string bodyModuleName = "BodyInfo";
-	std::string movementModuleName = "MovementTools";
-
-	// Set broker name, ip and port, finding first available port from 54000
-	const std::string brokerName = "MotionTimingBroker";
-	int brokerPort = qi::os::findAvailablePort(54000);
-	const std::string brokerIp = "0.0.0.0";
-	
-	// Default parent port and ip
-	int pport = 9559;
-	std::string pip = "127.0.0.1";
-	
-	// Need this for SOAP serialisation of floats to work
-	setlocale(LC_NUMERIC, "C");
-
-	// Create a broker
-	boost::shared_ptr<AL::ALBroker> broker;
-	try
-	{
-		broker = AL::ALBroker::createBroker(
-				brokerName,
-				brokerIp,
-				brokerPort,
-				pip,
-				pport,
-				0);
-	}
-	// Throw error and quit if a broker could not be created
-	catch(...)
-	{
-		std::cerr << "Failed to connect broker to: "
-			  << pip
-			  << ":"
-			  << pport
-			  << std::endl;
-		AL::ALBrokerManager::getInstance()->killAllBroker();
-		AL::ALBrokerManager::kill();
-
-		return 1;
-	}
-
-	// Add the broker to NAOqi
-	AL::ALBrokerManager::setInstance(broker->fBrokerManager.lock());
-	AL::ALBrokerManager::getInstance()->addBroker(broker);
-
-	CreateModule(movementLibName, movementModuleName, broker, false, true);
-	CreateModule(bodyLibName, bodyModuleName, broker, false, true);
-	
-	AL::ALProxy bodyInfoProxy(bodyModuleName, pip, pport);
-	AL::ALProxy movementToolsProxy(movementModuleName, pip, pport);
-	AL::ALMotionProxy motion(pip, pport);
-	//__________________________________________________________________________________________
-	//__________________________________________________________________________________________
-	//END OF STUFF WE DONT UNDERSTAND, BREATHE NOW
-	
+{
 	//learning factor
-	const double alpha=0.5;
+	const double alpha = 0.5;
 	//discount factor
-	const double gamma=0.5;
+	const double gamma = 0.5;
+	
+	const double deltatime = 0.1;
+	const double mass = 0.5;
+	const double length = 0.08;
+	const int angle_bins = 100;
+	const int velocity_bins = 50;
+	const int torque_bins = 9;
+	const int max_angle = M_PI;
+	const int max_velocity = 10;
+	const int max_torque = 4;
+	
+	Environment* env = new Environment(0, 0, 0, max_torque, 0, deltatime, mass, length, gamma);
 	
 	//seed rng
-	std::srand(std::time(NULL));
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
 	
-	int action_forwards = FORWARD;
-	int action_backwards = BACKWARD;
-	int chosen_action = action_forwards;
+	//create the possible actions as well as the chosen action
+	float chosen_action = 1;	// rip f
+	float actions[torque_bins];
+	for (int i = 0; i < (max_torque*2)+1 ; ++i)
+	{
+		actions[i] = static_cast<float>(-max_torque+i);
+	}
 	
 	//create a priority queue to copy to all the state space priority queues
-	PriorityQueue<int,double> initiator_queue(MAX);
-	initiator_queue.enqueueWithPriority(action_forwards,0);
-	initiator_queue.enqueueWithPriority(action_backwards,0);
-	
-	//create encoder
-	Encoder encoder();
-	encoder.calibrate();
+	PriorityQueue<float, double> initiator_queue(MAX);
+	for (int i = 0; i < torque_bins; ++i)
+	{
+		initiator_queue.enqueueWithPriority(actions[i], 0);
+	}
 	
 	//create the state space
-	StateSpace space(initiator_queue);
-	space.setAngle(100);
-	space.setVelocity(50);
+	StateSpace space(initiator_queue, angle_bins, velocity_bins, torque_bins, max_angle, max_velocity, max_torque);
 	
 	//state objects
-	State current_state(0,0,FORWARD);
-	State old_state(0,0,FORWARD);
+	State current_state(0, 0, 0);
+	State old_state(0, 0, 0);
 	
-	while(true)
+	std::ofstream file("output.txt");
+	file.precision(16);
+	
+	file << "Trialno" << "	" << "Time" << "		" << "Theta" << "	" << "Thetadot" << "		" << "Torque" << std::endl;
+	
+	double trialno = 1;
+	unsigned long i=0;
+	while (true)
 	{
-		current_state.theta= M_PI * encoder.GetAngle()/180;
-		current_state.theta_dot=(current_state.theta - old_state.theta)/700; //Needs actual time
-		current_state.robot_state=chosen_action;
+		current_state.theta = env->getTheta();
+		current_state.theta_dot = env->getThetadot();
+		current_state.torque = env->getTorque();
+		
+		std::cout << "State Read" << std::endl;
 		
 		updateQ(space, chosen_action, old_state, current_state, alpha, gamma);
 		
-		old_state=current_state;
+		std::cout << "Q Updated" << std::endl;
+
+		if (current_state.theta>2 * 3.1415 && current_state.theta_dot>2 * 3.1415 && env->getTime() >= 10) {
+			env->resetPendulum();
+			std::cout<<"->unsuccessful trial\n";
+			trialno++;
+			i = 0;
+			continue;
+		}
 		
-		chosen_action=selectAction(space[current_state]);
+		old_state = current_state;
 		
-		(chosen_action)?movementToolsProxy.callVoid("swingForwards"):movementToolsProxy.callVoid("swingBackwards");
+		chosen_action = selectAction(space[current_state],i);
+		std::cout << "Action Selected" << std::endl;
+		env->setTorque(chosen_action);
+		
+		env->propagate();
+		std::cout << "Environment Propagated\n" << std::endl;
+
+		file << trialno << "	  " << env->getTime() << "	  " << current_state.theta << "	  " << current_state.theta_dot << "	   " << current_state.torque << std::endl;
+		++i;
 	}
-	
+
+	file.close();
+	delete env;
 	return 1;
 }
 
-double temperature()
+double temperature(unsigned long t)
 {
-	return std::time(NULL);
+	return 100.0*std::exp((-8.0*t*t)/(2600.0*2600.0))+0.1;
 }
 
 //function is fed with a priority queue of action-values 
 //generates Boltzmann distribution of these action-values
 //and selects an action based on probabilities 
-int selectAction(PriorityQueue<int,double>& a_queue)
-{	
-	typedef PriorityQueue<int,double> PQ;
-	typedef std::vector< std::pair<int, double> > Vec_Pair;
-	typedef std::pair<int, double> Pair;
+float selectAction(PriorityQueue<float, double>& a_queue, unsigned long iterations)
+{
+	typedef std::vector<std::pair<float, double>> VecPair ; 
 	
-	double sum= 0;
-	int i = 0;
+	//turn priority queue into a vector of pairs
+	VecPair vec = a_queue.saveOrderedQueueAsVector();
 	
-	int size = a_queue.getSize();
-	Vec_Pair action_vec(size);
+    //sum for partition function 
+	double sum = 0;
 	
-	//Calculate partition function by iterating over action-values
-	for(PQ::const_iterator iter = a_queue.begin(),end=a_queue.end(); iter < end; ++iter)
+	// calculate partition function by iterating over action-values
+	for (VecPair::iterator iter = vec.begin(), end = vec.end(); iter < end; ++iter)
 	{
-		sum += std::exp((iter->second)/temperature());
-	}
-	//Calculate boltzmann factors for action-values
-	for(Vec_Pair::iterator it = action_vec.begin(),end=action_vec.end(); it < end; ++it)
-	{
-	    it->first = a_queue[i].first;
-	    it->second = std::exp(a_queue[i].second /temperature()) / sum;
-	    ++i;
+		sum += std::exp((iter->second) / temperature(iterations));
 	}
 	
-	//calculate cumulative probability distribution    
-	for(Vec_Pair::iterator it1 = action_vec.begin()++,it2 = action_vec.begin(),end=action_vec.end(); it < end; ++it1,++it2)
+	//compute Boltzmann factors for action-values and enqueue to vec
+	for (VecPair::iterator iter = vec.begin(); iter < vec.end(); ++iter)
 	{
-	    it1->second += it2->second;
+		iter->second = std::exp(iter->second / temperature(iterations)) / sum;
+	}
+	
+	//calculate cumulative probability distribution
+	for (VecPair::iterator iter = vec.begin()++, end = vec.end(); iter < end; ++iter)
+	{
+	//second member of pair becomes addition of its current value
+	//and that of the index before it
+	iter->second += (iter-1)->second;
 	}
 	
 	//generate RN between 0 and 1
-	double rand_num = static_cast<double>(rand()/ (RAND_MAX));
+	double rand_num = static_cast<double>(rand()) / RAND_MAX;
 	
-	//select action based on probability 
-	for(Vec_Pair::iterator it = action_vec.begin(),end=action_vec.end(); it < end; ++it)
+	// choose action based on random number relation to priorities within action queue
+	for (VecPair::iterator iter = vec.begin(), end = vec.end(); iter < end; ++iter)
 	{
-		//if RN falls within cumulative probability bin return the corresponding action
-		if(rand_num < it->second)return it->first;
+		if (rand_num < iter->second)return iter->first;
 	}
- 	
-	return NULL; //note that this line should never be reached
+	
+	return -10; //note that this line should never be reached	
 }
 
-void updateQ(StateSpace & space, int action, State & new_state, State & old_state, double alpha, double gamma)
+void updateQ(StateSpace & space, float action, State & new_state, State & old_state, double alpha, double gamma)
 {
-    //oldQ value reference
-    double oldQ = space[old_state].search(action).second;
-    
-    //reward given to current state 
-    double R = new_state.getReward();
-    
-    //optimal Q value for new state i.e. first element 
-    double maxQ = space[current_state].peekFront().second;
-    
-    //new Q value determined by Q learning algorithm
-    double newQ = oldQ + alpha * (R + (gamma * maxQ) - oldQ);
-    
-    // change priority of action to new Q value
-    space[old_state].changePriority(action, newQ);
+	//oldQ value reference
+	double oldQ = space[old_state].search(action).second;
+	
+	//reward given to current state 
+	double R = new_state.getReward();
+	
+	//optimal Q value for new state i.e. first element 
+	double maxQ = space[new_state].peekFront().second;
+	
+	//new Q value determined by Q learning algorithm
+	double newQ = oldQ + alpha * (R + (gamma * maxQ) - oldQ);
+	
+	space[old_state].changePriority(action, newQ);
 }
-
