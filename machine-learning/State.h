b547cf52deb94e4cbb6120da5269210b7daf3168
Ben <benjamin.stks@gmail.com>
2016-03-12 15:06:52 +0000

    Update State.h

--- machine-learning/State.h
+++ machine-learning/State.h
@@ -1,28 +1,23 @@
-#ifndef STATE_H_
-#define STATE_H_
+#ifndef STATE3_H_
+#define STATE3_H_
 
 #include <cmath>
 
- enum ROBOT_STATE {FORWARD, BACKWARD};
- const double g = 9.81;
-
 //class defines state object that holds angular position and velocity, and 
-//robot state. Has functions to calculate reward for a state
+//torque. Has functions to calculate reward for a state
 struct State
 {
-    //state variables
-    double theta;
-    double theta_dot;
-    ROBOT_STATE robot_state;
-    
-    //static const double g = 9.81;
-    
-    State(double _theta, double _theta_dot, ROBOT_STATE _robot_state);
-    
-    //function to get the reward of the stored state based on energy
-    double getReward();
-    //function to get the reward of the stored state based on height
-    double getHeightReward();
+	//state variables
+	double theta;
+	double theta_dot;
+	double torque;
+
+	static const double g;
+
+	State(double _theta, double _theta_dot, double _torque);
+
+	//reward function of current state
+	double getReward();
 };
 
 #endif
