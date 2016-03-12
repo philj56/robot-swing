d116dd60bcbae3f4e9fb8c4d1188f386ea93d0f4
Ben <benjamin.stks@gmail.com>
2016-03-12 15:07:32 +0000

    Update StateSpace.cpp

--- machine-learning/StateSpace.cpp
+++ machine-learning/StateSpace.cpp
@@ -1,34 +1,47 @@
 #include "StateSpace.h"
 
-// NOTE: cannot initialise angle_bins or velocity_bins here as they are static. Also space is not a field of StateSpace and
-// _angle_max and _velocity_max are not declared.
-StateSpace::StateSpace(PriorityQueue<int *,double> queue ):
-	space1(_angle_max, std::vector< PriorityQueue<int *,double> > (_velocity_max, PriorityQueue<int,double> (queue)))
-	space2(_angle_max, std::vector< PriorityQueue<int *,double> > (_velocity_max, PriorityQueue<int,double> (queue)))
-{}
+int StateSpace::angle_bins;
+int StateSpace::velocity_bins;
+int StateSpace::torque_bins;
+double StateSpace::angle_max;
+double StateSpace::velocity_max;
+double StateSpace::torque_max;
 
-StateSpace::SubscriptProxy1 StateSpace::operator[](const unsigned int robot_state)
+StateSpace::StateSpace(const PriorityQueue<float, double>& queue, int _angle_bins, int _velocity_bins, int _torque_bins, double _angle_max, double _velocity_max, double _torque_max) :
+	space(_angle_bins, std::vector<std::vector<PriorityQueue<float, double>>>(_velocity_bins, std::vector<PriorityQueue<float, double>>(_torque_bins, PriorityQueue<float, double>(queue))))
+
 {
-	//throw if the the index is out of bounds
-	if(robot_state>1)throw std::domain_error("action index exceeded");
-	//return proxy object to accept second [] operator
-	return SubscriptProxy1( robot_state ? space1 : space2 );
+	angle_bins=_angle_bins-1; //offset is to account for array indexes starting at 0
+	velocity_bins=_velocity_bins-1;
+	torque_bins=_torque_bins-1;
+	angle_max=_angle_max;
+	velocity_max=_velocity_max;
+	torque_max=_torque_max;
+}
+
+StateSpace::SubscriptProxy1 StateSpace::operator[](const double angle)
+{
+	//error if angle exceeds bounds
+	if ( std::abs(angle) > angle_max )
+	{
+		std::string error("angle argument exceeded with value: ");
+		error+=to_string(angle);
+		throw std::domain_error(error);
+	}
+	
+	//get the coefficient
+	double coef=0.5*angle_bins;
+	
+	//descretise index
+	int discrete_index = static_cast<int>( std::round( coef*(1+angle/angle_max) ) );
+	
+	//return appropriate object
+	return SubscriptProxy1(space[discrete_index]);
 }
 
 //searches state space by state object
-PriorityQueue<int *, double>& StateSpace::operator[](const State & state)
+PriorityQueue<float, double>& StateSpace::operator[](const State & state)
 {
-	//call the subscript operators with the members of the state object
-	return (*this)[state.robot_state][state.theta][state.theta_dot];
-}
-
-void setAngleBins(const double val)
-{
-	angle_bins=val;
-}
-
-void setVelocityBins(const double val)
-{
-	velocity_bins=val;
-}
+	//call the subscripts with the members of the state object
+	return (*this)[state.theta][state.theta_dot][state.torque];
 }
