1393823fef2f3a06823774bb356bd478d9972cf1
Ben <benjamin.stks@gmail.com>
2016-03-12 15:06:16 +0000

    Update State.cpp

--- machine-learning/State.cpp
+++ machine-learning/State.cpp
@@ -1,18 +1,14 @@
 #include "State.h"
-#include <cmath>        // std::abs
 
-State::State(double _theta, double _theta_dot, ROBOT_STATE _robot_state):
-  theta(_theta),
-  theta_dot(_theta_dot),
-  robot_state(_robot_state)
+const double State::g = 9.81;
+
+State::State(double _theta, double _theta_dot, double _torque) :
+	theta(_theta),
+	theta_dot(_theta_dot),
+	torque(_torque)
 {}
 
-double State::getReward(){
-  //return kinetic + potential
-  return g * std::cos(theta) + 0.5 * theta_dot * theta_dot;
-}
-
-double State::getHeightReward(){
-  //return height and sign of velocity, add 4 so it is always positive
-  return (theta * theta_dot)/std::abs(theta_dot) + 4;
+double State::getReward() {
+	//reward proportional to theta
+	return -std::cos(theta);
 }
