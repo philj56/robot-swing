0242358d4585a04b5afbf9eec4aee7081e3f3d37
SJR276 <sjrowlinson@outlook.com>
2016-03-12 14:21:30 +0000

    Fixed iterator access in selectAction

--- pendulum/main.cpp
+++ pendulum/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
@@ -23,8 +24,7 @@ float selectAction(PriorityQueue<float, double>& a_queue, unsigned long int iter
 //function to update a q value
 void updateQ(StateSpace & space, float  action, State & new_state, State & old_state, double alpha, double gamma);
 
-int main()
-{
+int main() {
 	//learning factor
 	const double alpha = 0.5;
 	//discount factor
@@ -32,7 +32,7 @@ int main()
 	const double deltatime = 0.1;
 	const double mass = 0.5;
 	const double length = 0.08;
-	
+
 	const int angle_bins = 100;
 	const int velocity_bins = 50;
 	const int torque_bins = 9;
@@ -41,7 +41,7 @@ int main()
 	const double maxtorque = 4;
 
 	/*for (int i = 0; i < torque_bins; ++i) {
-		const int t_i = -maxtorque + i;
+	const int t_i = -maxtorque + i;
 	}*/
 
 	environment* env = new environment(0, 0, 0, maxtorque, 0, deltatime, mass, length, gamma);
@@ -52,8 +52,8 @@ int main()
 	//create pointers to the possible actions as well as a pointer to hold the chosen action
 	float chosen_action = 1.0f;	// THE F MUST NOT BE DELETED UNDER ANY CIRCUMSTANCES
 	float actions[torque_bins];
-	for (int i = 0; i < (maxtorque*2)+1 ; ++i) {
-		actions[i] = static_cast<float>(-maxtorque+i);
+	for (int i = 0; i < (maxtorque * 2) + 1; ++i) {
+		actions[i] = static_cast<float>(-maxtorque + i);
 	}
 
 	//create a priority queue to copy to all the state space priority queues
@@ -75,33 +75,32 @@ int main()
 	file << "Trialno" << "	" << "Time" << "		" << "Theta" << "	" << "Thetadot" << "		" << "Torque" << std::endl;
 
 	double trialno = 1;
-	unsigned long i=0UL;
-	while (true)
-	{
+	unsigned long i = 0UL;
+	while (true) {
 		current_state.theta = env->getTheta();
 		current_state.theta_dot = env->getThetadot();
 		current_state.torque = env->getTorque();
 		std::cout << "State Read" << std::endl;
-		
+
 		updateQ(space, chosen_action, old_state, current_state, alpha, gamma);
 		std::cout << "Q Updated" << std::endl;
 
-		if (current_state.theta>2 * 3.1415 && current_state.theta_dot>2 * 3.1415 && env->getTime() >= 10) {
+		if (current_state.theta > 2*M_PI && current_state.theta_dot> 2*M_PI && env->getTime() >= 10) {
 			env->resetPendulum();
 			std::domain_error("unsuccessful trial");
 			trialno++;
 			i = 0;
 		}
-		
+
 		old_state = current_state;
-		
-		chosen_action = selectAction(space[current_state],i);
+
+		chosen_action = selectAction(space[current_state], i);
 		std::cout << "Action Selected" << std::endl;
 		env->setTorque(chosen_action);
-		
+
 		env->propagate();
 		std::cout << "Environment Propogated\n" << std::endl;
-		
+
 		file << trialno << "	  " << env->getTime() << "	  " << current_state.theta << "	  " << current_state.theta_dot << "	   " << current_state.torque << std::endl;
 		++i;
 	}
@@ -116,32 +115,35 @@ double temperature(unsigned long t) {
 }
 
 float selectAction(PriorityQueue<float, double>& a_queue, unsigned long iterations) {
-	
-	typedef std::vector<std::pair<float, double> > VecPair ; 
-	
+
+	typedef std::vector<std::pair<float, double> > VecPair;
+
 	//turn priority queue into a vector of pairs
 	VecPair vec = a_queue.saveOrderedQueueAsVector();
-
-    //sum for partition function 
+	std::cout << "\tqueue saved as vector" << std::endl;
+	//sum for partition function 
 	double sum = 0.0;
 
 	// calculate partition function by iterating over action-values
 	for (VecPair::iterator iter = vec.begin(), end = vec.end(); iter < end; ++iter) {
 		sum += std::exp((iter->second) / temperature(iterations));
 	}
-
+	std::cout << "\tpartition function computed" << std::endl;
 	// compute Boltzmann factors for action-values and enqueue to vec
 	for (VecPair::iterator iter = vec.begin(); iter < vec.end(); ++iter) {
 		iter->second = std::exp(iter->second / temperature(iterations)) / sum;
 	}
-
+	std::cout << "\tBoltzmann factors calculated" << std::endl;
 	// calculate cumulative probability distribution
-	for (VecPair::iterator iter = vec.begin()++, end = vec.end(); iter < end; ++iter) {
-        //second member of pair becomes addition of its current value
-        //and that of the index before it
-		iter->second += (iter-1)->second;
+	for (VecPair::iterator iter = vec.begin(), end = vec.end(); iter < end; ++iter) {
+		//second member of pair becomes addition of its current value
+		//and that of the index before it
+		if (iter == vec.begin()) {}
+		else {
+			iter->second += (iter - 1)->second;
+		}
 	}
-
+	std::cout << "\tCumulative PD computed" << std::endl;
 	//generate RN between 0 and 1
 	double rand_num = static_cast<double>(rand()) / RAND_MAX;
 
@@ -150,12 +152,11 @@ float selectAction(PriorityQueue<float, double>& a_queue, unsigned long iteratio
 		if (rand_num < iter->second)
 			return iter->first;
 	}
-	
+	std::cout << "\tAction chosen" << std::endl;
 	return -1; //note that this line should never be reached	
 }
 
-void updateQ(StateSpace & space, float action, State & new_state, State & old_state, double alpha, double gamma)
-{
+void updateQ(StateSpace & space, float action, State & new_state, State & old_state, double alpha, double gamma) {
 	//std::cout << action << std::endl;
 	//std::cout << space[old_state].toString() << std::endl << "-----------";
 
@@ -173,4 +174,3 @@ void updateQ(StateSpace & space, float action, State & new_state, State & old_st
 	//std::cout << "New Q-Value: " << newQ << std::endl;
 	space[old_state].changePriority(action, newQ);
 }
-
