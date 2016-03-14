4798c765c14bd69bbebcf3de6ba8d200dd0d39d6
Dan <dan@gs.(none)>
2016-03-14 06:32:12 -0400

    Updated humanswing output info

--- sdk/humanswing/main.cpp
+++ sdk/humanswing/main.cpp
@@ -157,7 +157,9 @@ int main(int argc, char* argv[])
 	if(verb)
 		std::cout << bold_on << "Creating encoder..." << bold_off << std::endl;
 	Encoder encoder;
-	
+
+	encoder.Calibrate();
+
 	float lastAngle;
 	float currentAngle = encoder.GetAngle();
 
@@ -166,8 +168,8 @@ int main(int argc, char* argv[])
 	float maxAngle = 1;
 
 	// Bools for whether the robot is moving forwards or backwards
-	bool forwards;
-	bool backwards;
+	bool forwards = false;
+	bool backwards = false;
 
 	// Get start time of simulation and store it
 	qi::os::timeval startTime;
@@ -188,22 +190,25 @@ int main(int argc, char* argv[])
 		forwards = currentAngle < lastAngle;
 		if (forwards == backwards)
 		{
+			float moveTime = 1000 * (static_cast<int>(currentTime.tv_sec) - static_cast<int>(startTime.tv_sec)) 
+			              + 0.001 * (static_cast<int>(currentTime.tv_usec) - static_cast<int>(startTime.tv_usec));
 			if (forwards)
 			{
 				minAngle = currentAngle;
-				std::cout << currentTime.tv_sec << "." << currentTime.tv_usec << ": minAngle = " << minAngle << std::endl; 
+				std::cout << moveTime << ": minAngle = " << minAngle << std::endl; 
 			}
 			else
 			{
 				maxAngle = currentAngle;
-				std::cout << currentTime.tv_sec << "." << currentTime.tv_usec << ": maxAngle = " << maxAngle << std::endl; 
+				std::cout << moveTime << ": maxAngle = " << maxAngle << std::endl; 
 			}
 		}
 		backwards = !forwards;
 
 		// Ensure humanSwing is only called with an angle between 0 and 1
+		std::cout << "Calling with value " << std::abs(currentAngle - minAngle) / std::abs(maxAngle - minAngle) << std::endl;
 		movementToolsProxy.callVoid("humanSwing", std::abs(currentAngle - minAngle) / std::abs(maxAngle - minAngle), forwards); 
-		qi::os::msleep(40);
+		qi::os::msleep(50);
 	}
 
 	// Get a handle to the module and close it
